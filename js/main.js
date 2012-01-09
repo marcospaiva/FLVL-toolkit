5227c5aa503fc61f5e475a5382a846c8464ad32f
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-01-09 16:09:45 -0200

    Modificando chamada de js para arquivo externo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-// 
-$(document).ready(function(){
+//init app
+$(function(){
+	// Carousel div
+	$("#side_loja .slider-navigation").carousel( { 		
+		pagination: true,
+		autoSlide: true,
+   	 	autoSlideInterval: 5000,
+  	  	direction: "horizontal",
+   	 	loop: true ,
+   	 	dispItems: 1,
+	} );
+	
 	$("#global_search").bind('mouseover mouseout',function(){
 	        $(this).children().children().toggleClass("submit_hover");
-	        //alert("fsodifuha")
 	    })
+	
 });
