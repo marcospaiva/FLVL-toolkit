c28f3f624c6a0f64380ee85efcd30feb43a7b598
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-01-10 08:32:11 -0200

    Revert 71665c1fc4b42a6fa90b3b14b01b5d305d67e1eb^..HEAD

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
-//init app
-$(function(){
-	// Carousel div
-	$("#side_loja .slider-navigation").carousel( { 		
-		pagination: true,
-		autoSlide: true,
-   	 	autoSlideInterval: 5000,
-  	  	direction: "horizontal",
-   	 	loop: true ,
-   	 	dispItems: 1,
-	} );
-	$(".slider-simple").carousel( { }
-	$("#global_search").bind('mouseover mouseout',function(){
-	        $(this).children().children().toggleClass("submit_hover");
-	    })
-	
+// 
+$(document).ready(function(){
+    $("#global_search .submit").bind('mouseover mouseout',function(){
+        $(this).prev().children().toggleClass("submit_hover");
+    })
 });
