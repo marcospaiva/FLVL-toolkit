029ed4abe51e0ffb88b442d9fa0b1c5d22831865
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-01-10 08:44:55 -0200

    merge branches

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+<<<<<<< HEAD
 // 
 $(document).ready(function(){
     $("#global_search .submit").bind('mouseover mouseout',function(){
         $(this).prev().children().toggleClass("submit_hover");
     })
 });
+=======
+//init app
+$(function(){
+	// Carousel div
+	$("#side_loja .slider-navigation").carousel( { 		
+		pagination: true,
+		autoSlide: true,
+   	 	autoSlideInterval: 5000,
+  	  	direction: "horizontal",
+   	 	loop: true ,
+   	 	dispItems: 1,
+	} );
+	$(".slider-simple").carousel( { 		
+		pagination: false,
+		autoSlide: false,
+  	  	direction: "vertical",
+   	 	loop: true ,
+   	 	dispItems: 3,
+	} );
+	
+	$("#global_search").bind('mouseover mouseout',function(){
+	        $(this).children().children().toggleClass("submit_hover");
+	    })
+	
+});
+>>>>>>> search
