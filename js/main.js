41bca94e40ebb82a5ad674071cc87c8aef699939
Marcos Paiva <eu.marcos.paiva@gmail.com>
2012-01-10 08:56:56 -0200

    Acertos na interface e alteração na chamada JS

--- js/main.js
+++ js/main.js
@@ -1,33 +1,26 @@
-<<<<<<< HEAD
-// 
-$(document).ready(function(){
-    $("#global_search .submit").bind('mouseover mouseout',function(){
-        $(this).prev().children().toggleClass("submit_hover");
-    })
-});
-=======
-//init app
-$(function(){
-	// Carousel div
-	$("#side_loja .slider-navigation").carousel( { 		
+//init app
+$(function(){
+	// Carousel div
+	$("#side_loja .slider-navigation").carousel( {
 		pagination: true,
 		autoSlide: true,
    	 	autoSlideInterval: 5000,
   	  	direction: "horizontal",
    	 	loop: true ,
    	 	dispItems: 1,
-	} );
-	$(".slider-simple").carousel( { 		
+	} );
+	$(".slider-simple").carousel( { 
 		pagination: false,
 		autoSlide: false,
+   	 	autoSlideInterval: 4500,
   	  	direction: "vertical",
-   	 	loop: true ,
+   	 	loop: false ,
    	 	dispItems: 3,
+		effect: "fade",
 	} );
-	
 	$("#global_search").bind('mouseover mouseout',function(){
 	        $(this).children().children().toggleClass("submit_hover");
 	    })
 	
 });
->>>>>>> search
+
